c72bbfabaceaaa741f7137eb3aeeed20b19ae14d
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-07-21 00:58:08 -0400

    another attemp

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,117 @@
-var titleTime = moment().format('llll');
-
+var titleTime = moment().format('dddd, MMMM Do');
+var hour = moment().format("h A")
 //current day displayed
 $("#currentDay").html(titleTime);
 
 var calendarList= [
     {
-        time: "9AM",
+        time: "9 AM",
         work:"",
     },
     {
-        time: "10AM",
+        time: "10 AM",
         work:"",
     },
     {
-        time: "11AM",
+        time: "11 AM",
         work:"",
     },
     {
-        time: "12AM",
+        time: "12 PM",
         work:"",
     },
     {
-        time: "1PM",
+        time: "1 PM",
         work:"",
     },
     {
-        time: "2PM",
+        time: "2 PM",
         work:"",
     },
     {
-        time: "3PM",
+        time: "3 PM",
         work:"",
     },
     {
-        time: "4PM",
+        time: "4 PM",
         work:"",
     },
     {
-        time: "5PM",
+        time: "5 PM",
         work:"",
     },
 ];
 
-$(function(){
+$('#9').text(calendarList[0].time);
+$('#10').text(calendarList[1].time);
+$('#11').text(calendarList[2].time);
+$('#12').text(calendarList[3].time);
+$('#1').text(calendarList[4].time);
+$('#2').text(calendarList[5].time);
+$('#3').text(calendarList[6].time);
 
-    calendarList.forEach(function(element, index){
 
-        // this var takes each time of the timelist obj and create a divs.
-        var hours = element.time;
+$(function color() {
+    var calendarT = $('#9').html();
+    
+    if (calendarT > hour) { 
+        $('#area9').removeClass('past');
+        $('#area9').removeClass('present');
+        $('#area9').addClass('future');
+        console.log(calendarT);
+    }
+    else if (calendarT = hour) {
+        $('#area9').removeClass('past');
+        $('#area9').removeClass('future');
+        $('#area9').addClass('present');
+    }
+    else if (calendarT < hour) {
+        $('#area9').removeClass('present');
+        $('#area9').removeClass('future');
+        $('#area9').addClass('past');
+        
+    }   
+})
+$(function color() {
+    var calendarT = $('#10').html();
+    
+    if (calendarT > hour) { 
+        $('#area10').removeClass('past');
+        $('#area10').removeClass('present');
+        $('#area10').addClass('future');
+        console.log(calendarT);
+    }
+    else if (calendarT = hour) {
+        $('#area10').removeClass('past');
+        $('#area10').removeClass('future');
+        $('#area10').addClass('present');
+    }
+    else if (calendarT < hour) {
+        $('#area10').removeClass('present');
+        $('#area10').removeClass('future');
+        $('#area10').addClass('past');
+        
+    }   
+})
 
-        $(".container").append("<div class = 'row time-block'> <div class='hour col-1'> "+ hours +
-        " </div> <textarea id='data-"+index+"' class='col-10 description '>"+element.work+"</textarea> <button class='btn saveBtn col-1' id='btn'> <span><i class= 'fa fa-save'></i></span></button>");
-    });
-});
+
+$( "button" ).click(function() {
+    var text = $( this ).text();
+    $( "input" ).val( text );
+    console.log(text);
+  });
+console.log(hour);
+// $('.hour').text(calendarList[0].time);
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with time blocks for standard business hours
+// WHEN I view the time blocks for that day
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+// WHEN I click into a time block
+// THEN I can enter an event
+// WHEN I click the save button for that time block
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
